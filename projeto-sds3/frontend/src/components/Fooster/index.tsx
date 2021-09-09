c9d095b2f4ffe335e03f185bd63eced43a0afac9
Sebastião Costa Maia Neto <sebastiaosoares2010@hotmail.com>
2021-09-09 10:29:38 -0300

    Basic static components

---
+++ projeto-sds3/frontend/src/components/Fooster/index.tsx
@@ -0,0 +1,13 @@
+const Fooster = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/sebastiacost" target="_blank" rel="noreferrer">Sebastião Costa Maia</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Fooster;
