bab819b9455ddf3f521981226d567db42fa475db
Sebastião Costa Maia Neto <sebastiaosoares2010@hotmail.com>
2021-09-11 21:14:10 -0300

    Group by search

--- projeto-sds3/backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
+++ projeto-sds3/backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSucessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepositories;
 import com.devsuperior.dsvendas.repositories.SellerRepositories;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepositories.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupeBySeller(){
+		return repository.amountGroupeBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSucessDTO> sucessGroupeBySeller(){
+		return repository.sucessGroupeBySeller();
 	}
 }
